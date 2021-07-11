13b4b8fdee753d3f426960c508a2747ac9af7b89
Patricio <patricioprp06@gmail.com>
2021-07-11 12:54:14 -0300

    se resuelve bug de login

--- hooks/useAutenticacion.js
+++ hooks/useAutenticacion.js
@@ -1,21 +1,20 @@
-import React, { useState, useEffect } from "react";
-import firebase from "../firebase";
+import React, { useEffect, useState } from 'react';
+import firebase from '../firebase';
 
 function useAutenticacion() {
-  const [usuarioAutenticado, guardarUsuarioAutenticado] = useState(null);
+    const [ usuarioAutenticado, guardarUsuarioAutenticado] = useState(null);
 
-  useEffect(() => {
-    const unsuscribe = firebase.auth.onAuthStateChanged(user => {
-      if (user) {
-        guardarUsuarioAutenticado(user);
-      } else {
-        guardarUsuarioAutenticado(null);
-      }
-    });
-    return unsuscribe();
-  }, []);
+    useEffect(() => {
+        const unsuscribe = firebase.auth.onAuthStateChanged(user => {
+            if( user ) {
+                guardarUsuarioAutenticado(user);
+            } else {
+                guardarUsuarioAutenticado(null);
+            }
+        });
+        return () => unsuscribe();
+    }, []);
 
-  return usuarioAutenticado;
+    return usuarioAutenticado;
 }
-
 export default useAutenticacion;
